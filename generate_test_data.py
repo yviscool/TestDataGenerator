c5b565e0e6461cfab387a8ad9036f48b8d0ebc9d
YungVenuz <5196666qwe@gmail.com>
2023-12-13 22:03:20 +0800

    修改: 改成自幂数样例

--- generate_test_data.py
+++ generate_test_data.py
@@ -104,26 +104,41 @@ def compile_if_needed(file_names):
 
 # 编译并生成测试数据
 def generate_test_data(file_names):
-    
+
     compile_if_needed(file_names)
+    
+    armstrong_numbers = [153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084, 548834, 1741725, 4210818, 9800817, 9926315, 24678050, 24678051, 88593477]
 
-    # 生成 5 个测试样例
-    for i in range(1, 6):
-        x = randint(1, 100)
-        y = randint(1, 100)
-
+    for i in range(1, 11):  # 生成10组测试数据
         io = IO(file_prefix="", data_id=i)
-        io.input_writeln([x, y])
-        io.input_writeln([x, y])
+
+        M = randint(1, 50)  # 随机生成1到100之间的整数，表示待判断的正整数个数
+        io.input_writeln(M)  # 写入待判断的正整数个数
+
+        armstrong_count = randint(2, len(armstrong_numbers)-1)  # 随机生成自幂数的个数，确保不超过待判断的正整数个数
+        non_armstrong_count = M - armstrong_count  # 计算非自幂数的个数
+
+        armstrong_numbers_subset = random.sample(armstrong_numbers, armstrong_count)  # 随机选取自幂数的子集
+        non_armstrong_numbers = []
+
+        while non_armstrong_count > 0:
+            n = randint(1, 10**8)  # 随机生成小于10^8的整数
+            if n not in armstrong_numbers and n not in non_armstrong_numbers:
+                non_armstrong_numbers.append(n)
+                non_armstrong_count -= 1
+
+        numbers = armstrong_numbers_subset + non_armstrong_numbers
+        random.shuffle(numbers)  # 将数字列表打乱顺序
+
+        for n in numbers:
+            io.input_writeln(n)  # 写入待判断的正整数
 
         os_name = os.name
-        # 以下代码需要保留, 不需要修改. 
         if os_name == 'nt':  # Windows
-            # 执行shell命令或二进制文件，把输入文件的内容通过stdin送入，获得stdout的内容生成输出文件
-            io.output_gen("std.exe")
+            io.output_gen("std.exe")  # 调用C++可执行文件生成输出文件
             print(f"已生成测试数据 {i}")
         else:  # Linux
-            io.output_gen("./std")
+            io.output_gen("./std")  # 调用C++可执行文件生成输出文件
             print(f"已生成测试数据 {i}")
 
 
